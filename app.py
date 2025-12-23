b94a0fc4e5ca92950c6de9a202ed0465a3be8ef7
Ujjwal Thakur <ujjwalthakur@Ujjwals-MacBook-Air.local>
2025-12-23 22:31:53 +0530

    Removed socketio to fix module not found error

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit, join_room  # Chat ke liye naya
+
 import pymysql.cursors
 import sys
 import os
